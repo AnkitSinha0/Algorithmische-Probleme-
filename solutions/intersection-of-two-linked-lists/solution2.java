5a8273c7b48ac30127d651c0ab44feedef52636b
Ankit Sinha <119648425+AnkitSinha0@users.noreply.github.com>
2026-08-22 19:41:54 +0530

    Add solution 2 for Intersection of Two Linked Lists (Easy)

---
+++ solutions/intersection-of-two-linked-lists/solution2.java
@@ -0,0 +1,14 @@
+// two pointer with TC O(n) and SC O(1)
+public class Solution {
+   public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    ListNode a = headA;
+    ListNode b = headB;
+
+    while (a != b) {
+        a = (a == null) ? headB : a.next;
+        b = (b == null) ? headA : b.next;
+    }
+
+    return a;
+}
+}
