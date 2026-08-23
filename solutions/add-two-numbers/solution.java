8d03b6e74c5b30fdb702b002ac3357cf500f0e04
Ankit Sinha <119648425+AnkitSinha0@users.noreply.github.com>
2026-08-23 16:36:40 +0530

    Add solution 1 for Add Two Numbers (Medium)

---
+++ solutions/add-two-numbers/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        int carry = 0;
+        ListNode listRes = new ListNode(0);
+        ListNode curr = listRes;
+        while (l1 != null || l2 != null) {
+
+            int a = l1 != null ? l1.val : 0;
+            int b = l2 != null ? l2.val : 0;
+            int sum = a + b + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return listRes.next;
+    }
+}
