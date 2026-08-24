36c805ed5e78c6db0392a5906963a7f07317fe8a
Ankit Sinha <119648425+AnkitSinha0@users.noreply.github.com>
2026-08-24 18:24:01 +0530

    Add solution 2 for Plus One (Easy)

---
+++ solutions/plus-one/solution2.java
@@ -0,0 +1,37 @@
+// class Solution {
+//     public int[] plusOne(int[] digits) {
+//         int sum = 0  ;
+//         int carry = 1 ;
+// List <Integer>  list = new ArrayList<>();
+//         for(int i = digits.length - 1  ; i >=0 ; i--){
+//             sum = (digits[i] + carry);
+//             list.add(0, sum%10);
+//             carry = sum / 10 ;
+
+//         }
+//         if(carry>0){
+//             list.add(0,carry);
+//         }
+
+//         return list.stream().mapToInt(Integer::intValue).toArray();
+//     }
+// }
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        for (int i = digits.length - 1; i >= 0; i--) {
+
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+
+        return result;
+    }
+}
