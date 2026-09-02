e762817c2b77521f50778b65a9b4ed3b107e410a
Ankit Sinha <119648425+AnkitSinha0@users.noreply.github.com>
2026-09-02 20:56:27 +0530

    Add solution 5 for Kth Missing Positive Number (Easy)

---
+++ solutions/kth-missing-positive-number/solution5.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int left = 0 ;
+        int right = arr.length - 1;
+       
+
+     
+        while(left < right){
+            int mid = left + (right - left)/2;
+            int mis = arr[mid] - ( mid + 1);
+       
+            if(mis >= k ){
+                right = mid;
+            }else{
+                left = mid  + 1;
+            }
+        }
+
+        int mis =  arr[right] -  (right + 1);
+     
+        if(mis >= k){
+            return arr[right] - ( mis - k + 1);
+        }else{
+            return arr[right] + (k- mis);
+        }
+
+    }
+}
+// class Solution {
+//     public int findKthPositive(int[] arr, int k) {
+//         int cnt = 1 ;
+//         int ind = 0 ;
+        
+//         while(ind  < arr.length && k != 0){
+//             if(arr[ind] == cnt){
+//                 ind++;
+//             }else{
+//                 k--;
+//             }
+//             cnt++;
+//         }
+
+//          if( k > 0){
+//             return cnt - 1 + k;
+//          } return cnt - 1;
+//     }
+// }
