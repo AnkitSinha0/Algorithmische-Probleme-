5503b464156cdac0e9ba0a3789c32c43b6a1ab23
Ankit Sinha <119648425+AnkitSinha0@users.noreply.github.com>
2026-09-05 18:56:43 +0530

    Add solution 2 for Rotate List (Medium)

---
+++ solutions/rotate-list/solution2.java
@@ -0,0 +1,75 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+        int n = 1;
+        ListNode tail = head;
+
+        while (tail.next != null) {
+            tail = tail.next;
+            n++;
+        }
+
+        k %= n;
+
+        if (k == 0) {
+            return head;
+        }
+
+        ListNode newTail = head;
+
+        for (int i = 0; i < n - k - 1; i++) {
+            newTail = newTail.next;
+        }
+
+        ListNode newHead = newTail.next;
+
+        newTail.next = null;
+
+        tail.next = head;
+
+        return newHead;
+    }
+}
+// class Solution {
+//     public ListNode rotateRight(ListNode head, int k) {
+//         if(head == null || head.next == null || k == 0){
+//             return head;
+//         }
+//         ListNode dummy = new ListNode(0);
+//         dummy.next = head;
+//         ListNode slow = dummy;
+//         ListNode fast = dummy;
+//         ListNode curr = dummy.next;
+//         int cnt  = 0;
+//         while(curr!=null){
+//             cnt++;
+//             curr = curr.next;
+//         }
+//         if(cnt == k || k % cnt  == 0 ){ return head;}
+//         for(int i = 0 ; i < k % cnt ; i++){
+//             fast=fast.next;
+//         }
+//         while(fast.next!=null){
+//             slow = slow.next;
+//             fast = fast.next;
+//         }
+//         ListNode temp = slow.next;
+//         slow.next =null;
+//         fast.next = dummy.next;
+//         dummy.next = temp;
+//         return dummy.next;
+//     }
+// }
