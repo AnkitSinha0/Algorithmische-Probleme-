088084122fb59f4c485509f782c270a5b4a81686
Ankit Sinha <119648425+AnkitSinha0@users.noreply.github.com>
2026-09-13 10:28:20 +0530

    Add solution 2 for Sqrt(x) (Easy)

---
+++ solutions/sqrtx/solution2.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int mySqrt(int x) {
+       if(x == 1 || x == 0){
+        return x;
+       }
+
+       int left = 1;
+       int right = x/2;
+       while(left <right){
+        int mid = left + (right - left + 1)/2;
+        if((long) mid * mid <= x){
+            left = mid;
+        }else{
+            right = mid - 1;
+        }
+       }
+
+       return left;
+}}
+
+// class Solution {
+//     public int mySqrt(int x) {
+//         if(x==0 || x==1){
+//             return x;
+//         }
+//         long ans = 0;
+//         int left = 0;
+//         int right = x/2;
+//         while(left<=right){
+//             int mid = left + (right - left)/2;
+//             if((long) mid*mid <= x){
+//                 ans = mid;
+//                 left = mid + 1;
+//             }else{
+//             right = mid - 1;
+//         }
+//     }
+//     return (int) ans;
+// }}
