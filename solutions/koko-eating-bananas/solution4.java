526a3b170845d3c058eaa22c5ad22d30051b9ab9
Ankit Sinha <119648425+AnkitSinha0@users.noreply.github.com>
2026-09-14 18:51:36 +0530

    Add solution 4 for Koko Eating Bananas (Medium)

---
+++ solutions/koko-eating-bananas/solution4.java
@@ -0,0 +1,93 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int mapile = Integer.MIN_VALUE;
+        long total = 0;
+         for (int i = 0; i < piles.length; i++) {
+            total += piles[i];
+        }
+
+        for(int pile : piles){
+            if(mapile < pile){
+                mapile = pile;
+            }
+        }
+      int left =  (int)((total + h - 1)/ h);
+      int right = mapile;
+      while(left < right){
+        int mid = left + (right - left) /2;
+        if(isValid(piles,mid,h))  right = mid;      
+        else left = mid + 1;
+    }
+    return right;  
+}
+      
+   
+
+    public boolean isValid(int [] piles , int k ,int h){
+        int time = 0;
+        for(int pile : piles){
+          
+        //     int div = pile / k;
+        //     time += div;
+        //     if(pile%k!=0) time++;
+        // }
+        time+=(pile + k - 1) / k ;
+
+       
+    }
+return h >= time;
+}
+}
+
+// class Solution {
+//     public int minEatingSpeed(int[] piles, int h) {
+
+//         int mapilePiles = 0;
+//         int hours = 0;
+//         for (int pile : piles) {
+//             mapilePiles = Math.mapile(pile, mapilePiles);
+//         }
+
+//         int low  =  1;
+//         int high =   mapilePiles;
+//         while(low<=high){
+//            int  mid = low +high /2;
+//             int hrs = 0;
+//             for (int pile : piles) {
+//                 hrs += (pile + mid - 1) / mid;
+//             }
+//             if(hrs <= h){
+//             hours = hrs;
+//             high =  mid - 1;
+//             }else{
+//                 low =  mid  + 1;
+//             }
+
+//         }
+       
+
+//         return hours;
+//     }
+// }
+// class Solution {
+//     public int minEatingSpeed(int[] piles, int h) {
+
+//         int mapilePiles = 0;
+
+//         for (int pile : piles) {
+//             mapilePiles = Math.mapile(pile, mapilePiles);
+//         }
+
+//         for (int mid = 1; mid <= mapilePiles; mid++) {
+//             long hrs = 0;
+//             for (int pile : piles) {
+//                 hrs += (pile + mid - 1) / mid;
+//             }
+//             if (hrs <= h) {
+//                 return mid;
+//             }
+//         }
+
+//         return -1;
+//     }
+// }
