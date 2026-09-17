306143bc7cc0191407b88abb493ad46bbedecc40
Ankit Sinha <119648425+AnkitSinha0@users.noreply.github.com>
2026-09-17 22:55:31 +0530

    Add solution 3 for Concatenation of Array (Easy)

---
+++ solutions/concatenation-of-array/solution3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int  n = nums.length;
+        int []  arr = new int[n*2];
+        for(int i = 0 ; i < n*2 ; i++){
+            arr[i] = nums[i%n];
+        }
+
+        return arr;
+    }
+}
+
+// class Solution {
+//     public int[] getConcatenation(int[] nums) {
+        
+//         int []  arr = new int[nums.length*2];
+
+//         System.arraycopy(nums,0,arr,0,nums.length);
+//         System.arraycopy(nums,0,arr,nums.length,nums.length);
+//         return arr;
+        
+//     }
+// }
