370b007ba47746fad6851ef536b909718491b322
Ankit Sinha <119648425+AnkitSinha0@users.noreply.github.com>
2026-09-19 19:52:44 +0530

    Add solution 1 for Merge Strings Alternately (Easy)

---
+++ solutions/merge-strings-alternately/solution.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int s1 = word1.length();
+        int s2 = word2.length();
+        
+        char []  ch = new char[s1 + s2];
+        int idx = 0 ;
+        int id1 = 0 ;
+        int id2 = 0 ;
+        while(id1 < s1 && id2 < s2){
+            if(idx%2 == 0){
+                ch[idx] = word1.charAt(id1);
+                idx++;
+                id1++;
+            }else{
+                ch[idx] = word2.charAt(id2);
+                idx++;
+                id2++;
+            }
+        }
+        while(id1<s1){
+            ch[idx] = word1.charAt(id1);
+            idx++;
+            id1++;
+        }
+           while(id2<s2){
+            ch[idx] = word2.charAt(id2);
+            idx++;
+            id2++;
+        }
+
+        return String.valueOf(ch);
+
+        // return new String(ch);
+
+    }
+}
