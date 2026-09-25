0b07a6026d2fde0b8de1da29c8bf1cbf7f047476
Ankit Sinha <119648425+AnkitSinha0@users.noreply.github.com>
2026-09-25 12:59:55 +0530

    Add solution 1 for Arranging Coins (Easy)

---
+++ solutions/arranging-coins/solution.java
@@ -0,0 +1,40 @@
+// class Solution {
+//     public int arrangeCoins(int n) {
+     
+//         for(int i = 1 ; i <= n ; i++){
+//             int currSum = (i*(i+1))/2;
+//             if (currSum > n){
+//                 return i-1;
+//             }else if( currSum == n ){
+//                 return i;
+//             }
+//         }
+
+//         return -1;
+//     }
+// }
+
+
+
+class Solution {
+    public int arrangeCoins(int n) {
+     
+       int left = 1;
+       int right = n;
+       while(left < right){
+        int mid = left + (right - left + 1)/2;
+        if(isValid(mid,n)){
+            left = mid;
+        }else{
+            right = mid - 1;
+        }
+       }
+
+       return left;
+    }
+
+
+    public boolean isValid(int x,int n){
+        return n >= (  (long) x*(x+1))/2 ;
+    }
+}
